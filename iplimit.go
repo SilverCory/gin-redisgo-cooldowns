317656a99e3224a203891bbb715aa3b56fe54d5c
Cory Redmond <ace@ac3-servers.eu>
2018-06-14 02:05:10 +0100

    Initial commit

---
+++ iplimit.go
@@ -0,0 +1,77 @@
+package gin_redisgo_cooldowns
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRateLimit(pool *redis.Pool, key string, requests uint, time time.Duration, keySuffixGetter func(*gin.Context) string) gin.HandlerFunc {
+
+	if keySuffixGetter == nil {
+		keySuffixGetter = KeySuffixGetterIP()
+	}
+
+	return func(ctx *gin.Context) {
+		// Initialise requestNumber at 0 because they're cool!
+		requestNumber := uint(0)
+
+		// Fetch the suffix the the redis key
+		// Return if it's empty.
+		keySuffix := strings.TrimSpace(keySuffixGetter(ctx))
+		if keySuffix == "" {
+			return
+		}
+
+		// Get and close the pool, also error check to make sure we have the pool.
+		c := pool.Get()
+		defer c.Close()
+		if err := c.Err(); err != nil {
+			panic(err)
+			return
+		}
+
+		// Get the actual number of requests from the database.
+		// Also increments. (INCR spits out the old value, and inserts if new)
+		reply, err := c.Do("INCR", key+keySuffix)
+		if err != nil {
+			panic(err)
+			return
+		} else {
+			requestNumber = reply.(uint)
+		}
+
+		// Check if the current number of requests is greater than allowed.
+		if requestNumber >= requests {
+			// Reset the expiry to give a fresh cooldown.
+			// Set expire once per cool down.
+			if requestNumber == requests {
+				c.Do("EXPIRE", key+keySuffix, time.Seconds())
+			}
+
+			// Abort and error
+			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"status":  http.StatusTooManyRequests,
+				"message": "too many requests",
+			})
+			return
+		} else if requestNumber <= 1 {
+			// If it's a new entry in redis, expire the key in a defined time.
+			c.Do("EXPIRE", key+keySuffix, time)
+		}
+
+		// Give them their request number.
+		ctx.Header("X-REQUEST-NUMBER", fmt.Sprint(requestNumber))
+		ctx.Next()
+	}
+}
+
+func KeySuffixGetterIP() func(*gin.Context) string {
+	return func(ctx *gin.Context) string {
+		return ctx.ClientIP()
+	}
+}
---
+++ iplimit_test.go
@@ -0,0 +1,63 @@
+package gin_redisgo_cooldowns
+
+import (
+	"github.com/SilverCory/gin-redisgo-cooldowns/redisutils"
+
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gin-gonic/gin"
+)
+
+func TestIpLimiter(t *testing.T) {
+	r := gin.Default()
+
+	redisPool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+		Dial: func() (redis.Conn, error) {
+			return redisutils.DialWithDB("tcp", "127.0.0.1:6379", "", "0")
+		},
+	}
+
+	r.Use(NewRateLimit(redisPool, "ratelimit.IP:", 100, time.Second*5, nil))
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
+
+	go r.Run(":9999")
+
+	for i := 0; i < 102; i++ {
+		c := &http.Client{}
+
+		resp, e := c.Get("http://127.0.0.1:9999")
+		if e != nil {
+			t.Error("Error during requests ", e.Error())
+			return
+		}
+
+		switch {
+		case i < 100:
+			break
+		case i == 100:
+			if resp.StatusCode != 429 {
+				t.Error("Threashold break not detected")
+			} else {
+				time.Sleep(time.Second * 5)
+			}
+			break
+		case i == 101:
+			if resp.StatusCode != 200 {
+				t.Error("Unnecessary block")
+			}
+			break
+		}
+	}
+}
